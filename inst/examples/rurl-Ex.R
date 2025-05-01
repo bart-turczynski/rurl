0181fe9f67c45015df12f0ed53ee374682761b50
Bart Turczynski <bartek@turczynski.pl>
2025-05-01 02:18:02 +0200

    devtools::check() valid; just a note on timestamp

--- inst/examples/rurl-Ex.R
+++ inst/examples/rurl-Ex.R
@@ -1,14 +1,11 @@
-# Example 1: Clean a URL without a scheme
-get_clean_url("example.com")
-
-# Example 2: Clean a URL with a scheme
+# Example 1: Clean a URL with a scheme
 get_clean_url("http://example.com")
 
-# Example 3: Get parse status of a URL
+# Example 2: Get parse status of a URL
 get_parse_status("http://example.com")
 
-# Example 4: Get domain of a URL
+# Example 3: Get domain of a URL
 get_domain("http://example.com/path")
 
-# Example 5: Get host of a URL
+# Example 4: Get host of a URL
 get_host("http://example.com/path")
